011db06bed4ec9ba28c4c708061e784e667be30d
tinyfive <tinyfive@mycolorway.com>
2011-03-02 01:15:19 +0800

    add indicator_analysis.html

--- assets/script/indicator.js
+++ assets/script/indicator.js
@@ -15,12 +15,29 @@ $(function(){
 			
 			//刷新数据
 			////
-			refreshIndicator();
+		}
+	}
+
+	function eventChangeDayOrMonth(e){
+		e.preventDefault();
+
+		var target = $(e.target);
+
+		if( target.hasClass("selected") ){
+			return
+		} else {
+			//切换选中的样式
+			$("#category-min a.selected").removeClass("selected");
+			target.addClass("selected");
+			
+			//刷新数据
+			////
 		}
 	}
 
 	$("#category a").bind("click", eventCategoryClick);
 	$("#areaSelector").select({"option": arrMapDict});
+	$("#category-min a").bind("click", eventChangeDayOrMonth);
 
 });
 
