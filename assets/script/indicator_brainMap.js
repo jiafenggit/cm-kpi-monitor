63e287a9c4c45f723bbcd0e6d4df9e2640545845
tinyfive <tinyfive@mycolorway.com>
2011-03-10 22:26:12 +0800

    support refresh the brainMap height onresize window

--- assets/script/indicator_brainMap.js
+++ assets/script/indicator_brainMap.js
@@ -132,7 +132,63 @@ brainMapJSON_test = [{
 	}]
 }];
 
+function _getPageSize() {
+	var xScroll, yScroll;
+	if (window.innerHeight && window.scrollMaxY) {	
+		xScroll = window.innerWidth + window.scrollMaxX;
+		yScroll = window.innerHeight + window.scrollMaxY;
+	} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
+		xScroll = document.body.scrollWidth;
+		yScroll = document.body.scrollHeight;
+	} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
+		xScroll = document.body.offsetWidth;
+		yScroll = document.body.offsetHeight;
+	}
+	var windowWidth, windowHeight;
+	if (self.innerHeight) {	// all except Explorer
+		if(document.documentElement.clientWidth){
+			windowWidth = document.documentElement.clientWidth; 
+		} else {
+			windowWidth = self.innerWidth;
+		}
+		windowHeight = self.innerHeight;
+	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+		windowWidth = document.documentElement.clientWidth;
+		windowHeight = document.documentElement.clientHeight;
+	} else if (document.body) { // other Explorers
+		windowWidth = document.body.clientWidth;
+		windowHeight = document.body.clientHeight;
+	}	
+	// for small pages with total height less then height of the viewport
+	if(yScroll < windowHeight){
+		pageHeight = windowHeight;
+	} else { 
+		pageHeight = yScroll;
+	}
+	// for small pages with total width less then width of the viewport
+	if(xScroll < windowWidth){	
+		pageWidth = xScroll;		
+	} else {
+		pageWidth = windowWidth;
+	}
+	arrayPageSize = new Array(pageWidth,pageHeight,windowWidth,windowHeight);
+	return arrayPageSize;
+};
+
+function calcBrainMapSize(){
+	
+	var viewHeight = _getPageSize()[3],
+		offset = $("#brainMap").offset(),
+		mapTop = offset.top;
+
+	$("#brainMap").height(viewHeight - mapTop - 40);
+}
+
 $(function(){
+	calcBrainMapSize();
+	window.onresize = calcBrainMapSize;
+			
+
 	$("#areaSelector").select({
 		option: arrMapDict, 
 		width: 300,
